db06e99a22fcec5162af5eef0afac020da4d4448
Hoang Lam Tran <lamtiensinh2301@gmail.com>
2026-03-14 11:09:53 +1100

    Initial project setup

---
+++ SmartPhotoSearch/App/SmartPhotoSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPhotoSearchApp.swift
+//  SmartPhotoSearch
+//
+//  Created by Lâm Trần on 13/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SmartPhotoSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
